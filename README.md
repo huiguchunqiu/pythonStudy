edd33a809aeb77babd132215a63ba1c7cd2bb872
wangxuyang <wxy2335962829@sina.com>
2020-04-23 17:22:02 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pythonStudy
 ## 学习python
+### 1、字符串
+  字符串方法
+ 
+  len() 长度
+
+  type() 类型
+
+
--- hellopython.py
+++ hellopython.py
@@ -6,3 +6,51 @@ kile = 'smile'
 tine = 'add'
 
 print(kile + '222' + tine)
+
+given_name = "Luffy"
+middle_names = 'D'
+family_name = "Monkey"
+
+name_length = len(given_name+middle_names+family_name)
+name_character_limit = 15
+print(name_length <= name_character_limit)
+
+'''
+注释
+'''
+"""
+这是注释
+"""
+# 这是注释
+# ”“”
+
+# 类型转化
+
+8
+8.0
+True
+'hello'
+
+print(type(8))
+print(type(8.0))
+print(type(True))
+print(type('hello'))
+
+print(float(8))
+print(int(8.0), int(8.4))
+
+print(bool(8), bool(True), bool(0), bool(-1), bool('hello'))
+
+print(int(True), int(False), str(True), float(True))
+
+print(int('92'), float('3'))
+
+# print(int('33132ss'))
+
+# 字符串方法
+
+# len 长度
+print(len('hello world'))
+# type 类型
+print(type('8'))
+
