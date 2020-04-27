169e95cec8724b8eb0e97593e6c3c9ac937025be
wangxuyang <wxy2335962829@sina.com>
2020-04-27 09:24:07 +0800

    4.27

--- README.md
+++ README.md
@@ -7,4 +7,20 @@
 
   type() 类型
 
-
+  capitalize() 第一个字符大写  
+  
+  string.count(str,beg=0,end=len(string))  str出现的次数
+  
+  string.endswith(str,beg=0,end=len(string)) 判断是否以str为结尾
+  
+  string.find(str,beg,end) 查找是否包含 返回索引值
+  
+  string.format() 格式化字符串
+  
+  string.index(str,beg,end) 查找是否包含 返回索引 不在会抛出一个错误
+  string.isalnum() 判断字符串是否至少包含一个字符并且全为数字或字母
+  string.isalpha() 判断字符串是否至少包含一个字符并且全为字母
+  
+  string.isdecimal() 判断字符串是否只包含10进制数字
+  string.isdigit() 判断是否只包含数字
+  
--- hellopython.py
+++ hellopython.py
@@ -11,7 +11,7 @@ given_name = "Luffy"
 middle_names = 'D'
 family_name = "Monkey"
 
-name_length = len(given_name+middle_names+family_name)
+name_length = len(given_name + middle_names + family_name)
 name_character_limit = 15
 print(name_length <= name_character_limit)
 
@@ -53,4 +53,59 @@ print(int('92'), float('3'))
 print(len('hello world'))
 # type 类型
 print(type('8'))
+# islower()
+Mom_side = 'Tom is a good boy'
+print(Mom_side.islower())
 
+print(Mom_side.count('o'))
+
+print('miss'.capitalize())
+
+print(Mom_side.find('233'))
+
+print(Mom_side.index('is'))
+
+print(Mom_side.isalnum())
+print('222'.isalnum(), 'sdk'.isalnum())
+
+coming = 1
+
+print(coming)
+
+coming = 2
+
+print(coming)
+
+del coming
+
+print('十六点222sss'.islower())
+print('十六点222sss2分色法 3DG2 '.islower())
+print('HELLO world'.islower())
+print('hello soo 23中'.islower())
+
+print('322'.isnumeric())
+print('23ss'.isnumeric())
+print('23s sds'.isspace())
+print(' '.isspace())
+str1 = '1..'
+sms = ('a', 'b', 'c')
+print(str1.join(sms))
+sqs = ['2', '2', 'sw']
+print(str1.join(sqs))
+
+
+a = input('input:')
+
+print(a)
+
+i = 2
+while i < 100:
+    j = 2
+    while j <= i / j:
+        if not (i % j):
+            break
+        j = j + 1
+    if j > i / j:
+        print(i, ' 是素数')
+    i = i + 1
+print("Good bye!")
